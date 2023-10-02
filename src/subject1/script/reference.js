95784342dc7f58b2f9859d8e08dd26e98a427734
“Sohhdee” <thrha1103@gmail.comgit config --global user.name Sohhdeegit config --global user.email thrha1103@gmail.com>
2023-10-03 01:28:41 +0900

    미어수정

--- src/subject1/script/reference.js
+++ src/subject1/script/reference.js
@@ -1,9 +1,11 @@
-const reference = [
-  {
-    author: "",
-    title: "",
-    year: "",
-    sourceTitle: "",
-    link: "",
-  },
-];
+```javascript
+  const reference = [
+    {
+      author: "김나은",
+      title: "201921405",
+      year: "",
+      sourceTitle: "",
+      link: "",
+    },
+  ]
+  ```;
